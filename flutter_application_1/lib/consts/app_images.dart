1828d37f7514c98b691e55fd85f136c41562ab55
Jaka Usenova <97898976+Jakausenova1@users.noreply.github.com>
2023-01-19 00:45:19 +0600

    buttons

--- flutter_application_1/lib/consts/app_images.dart
+++ flutter_application_1/lib/consts/app_images.dart
@@ -1,4 +1,6 @@
 abstract class AppImages {
   static const String bg = "assets/bg.jpg";
   static const String tura = "assets/tura.png";
+  static const String fb = "assets/fb.png";
+  static const String google = "assets/google.png";
 }
--- flutter_application_1/lib/ui/pages/login_screen.dart
+++ flutter_application_1/lib/ui/pages/login_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/consts/app_colors.dart';
 import 'package:flutter_application_1/consts/app_fonts.dart';
 
+import '../../consts/app_images.dart';
 import '../widgets/app_auth_text_field.dart';
+import '../widgets/app_border_button.dart';
 import '../widgets/app_elevated_button.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -13,18 +15,10 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        AppAuthTextField(
-          text: 'EMAIL OR PHONE',
-          hintText: "Loremipsum@gmail.com",
-          controller: controllerEmail,
-          onChanged: (String) {},
-        ),
-        AppElevatedButton(
-          title: "Log In",
-          onPressed: () {},
-        )
-      ]),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
+      ),
     );
   }
 }
