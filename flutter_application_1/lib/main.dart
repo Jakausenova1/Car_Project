e5af153a45958a6f7d1f5de8b93a1bbd634f0cde
Jaka Usenova <97898976+Jakausenova1@users.noreply.github.com>
2023-01-19 01:16:10 +0600

    buttons

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/ui/pages/login_page.dart';
 import 'package:flutter_application_1/ui/pages/welcome_page.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
