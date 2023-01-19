b319758336376a168b148e48b663ffccb3cc7ce4
bigli <daniel.kg9526@gmail.com>
2023-01-19 20:59:07 +0600

    add SingleChildScrollView, and add physics to Grid

--- flutter_application_1/lib/ui/pages/home_page.dart
+++ flutter_application_1/lib/ui/pages/home_page.dart
@@ -63,116 +63,120 @@ class _HomePageState extends State<HomePage> {
             )
           ],
         ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                InkWell(
-                  onTapDown: (_) {
-                    spreadRadius = 0;
-                    blurRadius = 0;
-                    setState(() {});
-                  },
-                  onTapUp: (_) {
-                    spreadRadius = 2;
-                    blurRadius = 3;
-                    setState(() {});
-                  },
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) => AlertDialog(
-                        content: IconButton(
-                          icon: const Icon(Icons.close),
-                          onPressed: () {
-                            Navigator.pop(context);
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    InkWell(
+                      onTapDown: (_) {
+                        spreadRadius = 0;
+                        blurRadius = 0;
+                        setState(() {});
+                      },
+                      onTapUp: (_) {
+                        spreadRadius = 2;
+                        blurRadius = 3;
+                        setState(() {});
+                      },
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            content: IconButton(
+                              icon: const Icon(Icons.close),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 100),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                spreadRadius: spreadRadius,
+                                blurRadius: blurRadius,
+                                color: AppColors.black.withOpacity(0.6),
+                              )
+                            ]),
+                        child: Image.asset(AppImages.banner),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustomChip(
+                          label: labels[0],
+                          selected: currentLabel == 0,
+                          onSelected: (isSelected) {
+                            currentLabel = 0;
+                            setState(() {});
                           },
                         ),
-                      ),
-                    );
-                  },
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 100),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [
-                          BoxShadow(
-                            spreadRadius: spreadRadius,
-                            blurRadius: blurRadius,
-                            color: AppColors.black.withOpacity(0.6),
-                          )
-                        ]),
-                    child: Image.asset(AppImages.banner),
-                  ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CustomChip(
-                      label: labels[0],
-                      selected: currentLabel == 0,
-                      onSelected: (isSelected) {
-                        currentLabel = 0;
-                        setState(() {});
-                      },
+                        CustomChip(
+                          label: labels[1],
+                          selected: currentLabel == 1,
+                          onSelected: (isSelected) {
+                            currentLabel = 1;
+                            setState(() {});
+                          },
+                        ),
+                        CustomChip(
+                          label: labels[2],
+                          selected: currentLabel == 2,
+                          onSelected: (isSelected) {
+                            currentLabel = 2;
+                            setState(() {});
+                          },
+                        ),
+                        const Icon(Icons.search),
+                      ],
                     ),
-                    CustomChip(
-                      label: labels[1],
-                      selected: currentLabel == 1,
-                      onSelected: (isSelected) {
-                        currentLabel = 1;
-                        setState(() {});
-                      },
+                    const SizedBox(
+                      height: 60,
                     ),
-                    CustomChip(
-                      label: labels[2],
-                      selected: currentLabel == 2,
-                      onSelected: (isSelected) {
-                        currentLabel = 2;
-                        setState(() {});
-                      },
-                    ),
-                    const Icon(Icons.search),
-                  ],
-                ),
-                const SizedBox(
-                  height: 60,
-                ),
-                Text(
-                  "Cars Available Near You",
-                  style: AppFonts.w400s20.copyWith(color: AppColors.black),
-                ),
-                Row(
-                  children: [
-                    const Spacer(),
                     Text(
-                      "View more",
-                      style: AppFonts.w400s12.copyWith(color: AppColors.red),
+                      "Cars Available Near You",
+                      style: AppFonts.w400s20.copyWith(color: AppColors.black),
+                    ),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        Text(
+                          "View more",
+                          style: AppFonts.w400s12.copyWith(color: AppColors.red),
+                        ),
+                      ],
+                    ),
+                    GridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: data.length,
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      ),
+                      itemBuilder: (BuildContext context, int index) {
+                        return CarCard(
+                            image: data[index].image,
+                            price: data[index].price,
+                            title: data[index].title);
+                      },
+                      
                     ),
                   ],
                 ),
-                Expanded(
-                  child: GridView.builder(
-                    itemCount: data.length,
-                    shrinkWrap: true,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                    ),
-                    itemBuilder: (BuildContext context, int index) {
-                      return CarCard(
-                          image: data[index].image,
-                          price: data[index].price,
-                          title: data[index].title);
-                    },
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ));
